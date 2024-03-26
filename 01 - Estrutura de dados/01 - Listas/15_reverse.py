c599c77a74c53167b81097e38a1d86a73f9ca33c
Edelson Santos <edelsonjr.4@hotmail.com>
2024-03-26 20:53:35 -0300

    Update 15_reverse.py

--- 01 - Estrutura de dados/01 - Listas/15_reverse.py
+++ 01 - Estrutura de dados/01 - Listas/15_reverse.py
@@ -3,3 +3,10 @@ linguagens = ["python", "js", "c", "java", "csharp"]
 linguagens.reverse()
 
 print(linguagens)  # ["csharp", "java", "c", "js", "python"]
+
+
+nome = ["E", "d", "e", "l", "s", "o", "n"]
+
+nome.reverse()
+nome.join("")
+print(nome)
